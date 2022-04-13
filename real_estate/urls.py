aefb96ee7f52681f45692726e1f170c9cc9c3c7a
Dawit Getachew <dawit.getachew1@gmail.com>
2022-04-14 01:22:00 +0300

    test: all routes work in containers

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -4,10 +4,13 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("supersecret/", admin.site.urls),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.jwt")),
     path("api/v1/profile/", include("apps.profiles.urls")),
+    path("api/v1/properties/", include("apps.properties.urls")),
+    path("api/v1/ratings/", include("apps.ratings.urls")),
+    path("api/v1/enquiries/", include("apps.enquiries.urls")),
 ]
 
 admin.site.site_header = "Real Estate Admin"
